ca45b7e959b567cb7fc97039a4a65da443f3ad94
Valerka-6 <valerka7187868@list.ru>
2024-02-23 17:25:23 +0300

    Домашнее задание 4

--- script.js
+++ script.js
@@ -7,3 +7,26 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+const sendInput = document.querySelector('#input');
+const itemsList = document.querySelector('.items');
+
+
+sendInput.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        const itemsListiesText = sendInput.value;
+
+        const itemNew = document.createElement('div');
+        itemNew.classList.add('element')
+        itemNew.textContent = itemsListiesText;
+        itemNew.addEventListener('click', function() {
+            itemNew.classList.toggle('done');
+        })
+
+
+        if (itemsListiesText != '') {
+            itemsList.append(itemNew);
+        }
+
+        sendInput.value = '';
+    }
+})
